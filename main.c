c4a197c9eb94a0a551ef021ad4c4d4b57cdbc309
Ann Neumann <bosh.anastasia@gmail.com>
2018-01-09 18:34:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,76 +1,73 @@
+#include "stdafx.h"
 #include <stdio.h>
 #include <malloc.h>
+#include "word_keeper.h"
 
-struct word_keeper {
-	char *word;
-	int n;
-};
-
-void quickSort(struct word_keeper *numbers, int left, int right){
-	int buffer; // разрешающий элемент
+void quickSort(struct word_keeper *numbers, int left, int right) { 
+	int buffer;
 	int l_hold = left;
 	int r_hold = right;
 	buffer = numbers[left].n;
 	char *buffer_w = numbers[left].word;
 
-	while (left < right){ // пока границы не сомкнутся
+	while (left < right) {
 		while ((numbers[right].n >= buffer) && (left < right))
-			right--; // сдвигаем правую границу пока элемент right больше buffer
-		if (left != right){
-			numbers[left].n = numbers[right].n; // перемещаем элемент right на место разрешающего
+			right--;
+		if (left != right) {
+			numbers[left].n = numbers[right].n;
 			numbers[left].word = numbers[right].word;
-			left++; // сдвигаем левую границу вправо
+			left++;
 		}
 		while ((numbers[left].n <= buffer) && (left < right))
-			left++; // сдвигаем левую границу пока элемент left меньше buffer
-		if (left != right){ // если границы не сомкнулись
-			numbers[right].n = numbers[left].n; // перемещаем элемент left на место right
+			left++;
+		if (left != right) {
+			numbers[right].n = numbers[left].n;
 			numbers[right].word = numbers[left].word;
-			right--; // сдвигаем правую границу вправо
+			right--;
 		}
 	}
-	numbers[left].n = buffer; // ставим разрешающий элемент на место
+	numbers[left].n = buffer;
 	numbers[left].word = buffer_w;
 	buffer = left;
 	left = l_hold;
 	right = r_hold;
-	if (left < buffer) // Рекурсивно вызываем сортировку для левой и правой части массива
+	if (left < buffer)
 		quickSort(numbers, left, buffer - 1);
 	if (right > buffer)
 		quickSort(numbers, buffer + 1, right);
 }
 
-int isValidSymbol(char text){
+int isValidSymbol(char text) {
 	return text == ' ' || text == '.' || text == 10 || text == 0 || text == '?' || text == '!'
 		|| text == ':' || text == ';' || text == ',' || text == -30 || text == -62 || text == -80
 		|| text == '\"'
 		|| (text >= 1 && text <= 64) || (text >= 91 && text <= 96) || (text >= 123 && text <= 255);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 	int max = 1000;
 	struct word_keeper *Arr = (struct word_keeper*)malloc(max * sizeof(struct word_keeper));
 
 	char text;
 
 	int N = 0;
-	int S_N = 0;
+	int countSentences = 0;
 
 	int start = 0;
 
 	int flag = -1;
 
 
-	FILE *inputFile = fopen("dune.txt", "r");
-	if (inputFile == NULL){
+	FILE *inputFile = fopen("dune.txt", "r"); 
+	if (inputFile == NULL) {
 		printf("Input file not found\n");
 		return 1;
 	}
 
 	FILE *outputFile = fopen("output.txt", "w");
 
-	for (int i1 = 0, j1 = 0; ;){
-		text = getc(inputFile);//printf("%c", text);
+	for (int i1 = 0, j1 = 0; ;) {
+		text = getc(inputFile);
 
 		if (text == EOF) break;
 
@@ -84,84 +81,83 @@ int main(int argc, char *argv[]){
 			j1++;
 		}
 		else {
-			if ((text == '.' || text == '!' || text == '?') && start){
-				S_N++;
+			if ((text == '.' || text == '!' || text == '?') && start) {
+				countSentences++;
 			}
 
-			if (!isValidSymbol(text)){
+			if (!isValidSymbol(text)) {
 				printf("ERROR)%d\n", text);
 				return 0;
 			}//else{printf("!%c", text);}
 
-            if(start){
+			if (start) {
 
-			Arr[i1].word[j1] = 0;
-			if (N == 0) {
-				//Arr_N[i1]=1;
-				Arr[i1].n = 1;
-				i1++;
-				j1 = 0;
-				start = 0;
-				N++;
-				continue;
-			}
+				Arr[i1].word[j1] = 0;
+				if (N == 0) {
+					//Arr_N[i1]=1;
+					Arr[i1].n = 1;
+					i1++;
+					j1 = 0;
+					start = 0;
+					N++;
+					continue;
+				}
 
-			for (int i = 0; i < N; i++){
-				if (Arr[i].word[0] == Arr[i1].word[0] || Arr[i].word[0] == Arr[i1].word[0] - 32 || Arr[i].word[0] - 32 == Arr[i1].word[0]) {
-					for (int j = 1;; j++){
-						if (Arr[i].word[j] != Arr[i1].word[j]) break;
-						if (Arr[i].word[j] == 0 && Arr[i1].word[j1] == 0) {
-							flag = i;
-							break;
+				for (int i = 0; i < N; i++) {
+					if (Arr[i].word[0] == Arr[i1].word[0] || Arr[i].word[0] == Arr[i1].word[0] - 32 || Arr[i].word[0] - 32 == Arr[i1].word[0]) {
+						for (int j = 1;; j++) {
+							if (Arr[i].word[j] != Arr[i1].word[j]) break;
+							if (Arr[i].word[j] == 0 && Arr[i1].word[j1] == 0) {
+								flag = i;
+								break;
+							}
 						}
 					}
+					if (flag != -1) {
+						Arr[flag].n++;
+						flag = 999999;
+						break;
+					}
 				}
-				if (flag != -1) {
-					Arr[flag].n++;
-					flag = 999999;
-					break;
+				
+				if (flag == 999999) {
+					j1 = 0;
+					flag = -1;
+					start = 0;
 				}
-			}
+				else {
+					Arr[i1].n = 1;
+					i1++;
+					j1 = 0;
+					start = 0;
+					N++;
+					if (N == max) {
+						struct word_keeper *p = (struct word_keeper*)malloc(max * 2 * sizeof(struct word_keeper));
+						max = 2 * max;
+						for (int i = 0; i < N; i++)
+							p[i] = Arr[i];
 
-			if (flag == 999999) {
-				j1 = 0;
-				flag = -1;
-				start = 0;
-			}
-			else {
-				Arr[i1].n = 1;
-				i1++;
-				j1 = 0;
-				start = 0;
-				N++;
-				if (N == max){
-					struct word_keeper *p = (struct word_keeper*)malloc(max * 2 * sizeof(struct word_keeper));
-					max = 2 * max;
-					for (int i = 0; i < N; i++)
-						p[i] = Arr[i];
-
-					free(Arr);
-					Arr = p;
+						free(Arr);
+						Arr = p;
+					}
 				}
-			}
 
-            }
+			}
 
 		}
 	}
-
 	quickSort(Arr, 0, N - 1);
 
-	fprintf(outputFile, "Total sentences: %d\n", S_N);
+	fprintf(outputFile, "Total sentences: %d\n", countSentences);
 
-	for (int i = 0; i < N; i++){
+	for (int i = 0; i < N; i++) {
 		fprintf(outputFile, "\n%s\t%d", Arr[i].word, Arr[i].n);
 	}
 
 	fclose(inputFile);
 	fclose(outputFile);
 
-	for(int i=0;i<N;i++)
+	for (int i = 0; i<N; i++)
 		free(Arr[i].word);
 	free(Arr);
 
