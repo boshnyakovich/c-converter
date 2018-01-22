c161a762d7acdbc1a77a78bbdedf45b9513e15a4
Ann Neumann <bosh.anastasia@gmail.com>
2018-01-22 08:57:42 +0300

    Update main.c
    -added library function qsort;
    -added check for output;
    -memory allocation for a word is implemented using the function calloc;
    -the algorithm for counting words is improved (used the function stricmp).

--- main.c
+++ main.c
@@ -1,43 +1,67 @@
-#include "word_keeper.h"
 #include <stdio.h>
 #include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
 
-void quickSort(struct word_keeper *numbers, int left, int right) { 
-	int buffer;
-	int l_hold = left;
-	int r_hold = right;
-	buffer = numbers[left].n;
-	char *buffer_w = numbers[left].word;
 
-	while (left < right) {
-		while ((numbers[right].n >= buffer) && (left < right))
-			right--;
-		if (left != right) {
-			numbers[left].n = numbers[right].n;
-			numbers[left].word = numbers[right].word;
-			left++;
-		}
-		while ((numbers[left].n <= buffer) && (left < right))
-			left++;
-		if (left != right) {
-			numbers[right].n = numbers[left].n;
-			numbers[right].word = numbers[left].word;
-			right--;
-		}
-	}
-	numbers[left].n = buffer;
-	numbers[left].word = buffer_w;
-	buffer = left;
-	left = l_hold;
-	right = r_hold;
-	if (left < buffer)
-		quickSort(numbers, left, buffer - 1);
-	if (right > buffer)
-		quickSort(numbers, buffer + 1, right);
+struct word_keeper {
+	char *word;
+	int n;
+};
+
+
+int insert(struct word_keeper *Arr, int Arr_size, char *word, int word_size, int left, int right){ 
+    if(right-left == 1){
+        if(stricmp(word, Arr[left].word) == 0){
+            Arr[left].n++;
+            return 0;
+        }
+        else{
+            if(stricmp(word, Arr[right].word) == 0){
+                Arr[right].n++;
+                return 0;
+            }
+            else{
+                for(int i = Arr_size; i > right; i--){
+                    Arr[i] = Arr[i-1];
+                }
+                Arr[right].word = (char*)malloc(word_size);
+                for(int i = 0; i < word_size; i++){
+                    Arr[right].word[i] = word[i];
+                }
+
+                Arr[right].n = 1;
+                return 1;
+            }
+        }
+    }
+
+    int ret = 0;
+
+    int center = (int)((right-left)/2+left);
+    if(stricmp(word, Arr[center].word) < 0){
+        ret = insert(Arr, Arr_size, word, word_size, left, center);
+    }
+    else{
+        if(stricmp(word, Arr[center].word) > 0){
+            ret = insert (Arr, Arr_size, word, word_size, center, right);
+        }
+        else{
+            Arr[center].n++;
+            return 0;
+        }
+    }
+    return ret;
 }
 
+
+int compare(const void * x1, const void * x2){
+    return ( *(int*)(x1+sizeof(char*)) - *(int*)(x2+sizeof(char*)) );              // если результат вычитания равен 0, то числа равны, < 0: x1 < x2; > 0: x1 > x2
+}
+
+
 int isInvalidSymbol(unsigned char text) {
-	return text == ' ' || text == '.' || text == 10 || text == 0 || text == '?' || text == '!'
+	return text == ' ' || text == '.' || text == 10  || text == 0 || text == '?' || text == '!'
 		|| text == ':' || text == ';' || text == ',' || text == -30 || text == -62 || text == -80
 		|| text == '\"'
 		|| (text >= 1 && text <= 64) || (text >= 91 && text <= 96) || (text >= 123 && text <= 255);
@@ -45,38 +69,40 @@ int isInvalidSymbol(unsigned char text) {
 
 int main(int argc, char *argv[]) {
 	int max = 1000;
-	struct word_keeper *Arr = (struct word_keeper*)malloc(max * sizeof(struct word_keeper));
+	struct word_keeper *Arr = (struct word_keeper*)calloc(max, sizeof(struct word_keeper));
 
 	char text;
+
+	char buff[100];
+
 
 	int N = 0;
 	int countSentences = 0;
 
 	int start = 0;
 
-	int flag = -1;
 
-
-	FILE *inputFile = fopen("dune.txt", "r"); 
+	FILE *inputFile = fopen("dune.txt", "r");
 	if (inputFile == NULL) {
 		printf("Input file not found\n");
 		return 1;
 	}
 
 	FILE *outputFile = fopen("output.txt", "w");
+	if (outputFile == NULL) {
+		printf("Output file was not created\n");
+		return 1;
+	}
 
 	for (int i1 = 0, j1 = 0; ;) {
 		text = getc(inputFile);
 
-		if (text == EOF) break;
-
 		if (text >= 'a' && text <= 'z' || text >= 'A' && text <= 'Z') {
 			if (start == 0) {
-				Arr[i1].word = (char*)malloc(50 * sizeof(char));
 				start = 1;
 			}
 
-			Arr[i1].word[j1] = text;
+			buff[j1] = text;
 			j1++;
 		}
 		else {
@@ -91,9 +117,13 @@ int main(int argc, char *argv[]) {
 
 			if (start) {
 
-				Arr[i1].word[j1] = 0;
+                buff[j1] = 0;
 				if (N == 0) {
-					//Arr_N[i1]=1;
+                    Arr[i1].word = (char*)malloc(j1+1);
+                    for(int i = 0; i <= j1; i++){
+                        Arr[i1].word[i] = buff[i];
+                    }
+
 					Arr[i1].n = 1;
 					i1++;
 					j1 = 0;
@@ -102,50 +132,68 @@ int main(int argc, char *argv[]) {
 					continue;
 				}
 
-				for (int i = 0; i < N; i++) {
-					if (Arr[i].word[0] == Arr[i1].word[0] || Arr[i].word[0] == Arr[i1].word[0] - 32 || Arr[i].word[0] - 32 == Arr[i1].word[0]) {
-						for (int j = 1;; j++) {
-							if (Arr[i].word[j] != Arr[i1].word[j]) break;
-							if (Arr[i].word[j] == 0 && Arr[i1].word[j1] == 0) {
-								flag = i;
-								break;
-							}
-						}
-					}
-					if (flag != -1) {
-						Arr[flag].n++;
-						flag = 999999;
-						break;
-					}
-				}
-				
-				if (flag == 999999) {
-					j1 = 0;
-					flag = -1;
-					start = 0;
-				}
-				else {
-					Arr[i1].n = 1;
-					i1++;
-					j1 = 0;
-					start = 0;
-					N++;
-					if (N == max) {
-						struct word_keeper *p = (struct word_keeper*)malloc(max * 2 * sizeof(struct word_keeper));
-						max = 2 * max;
-						for (int i = 0; i < N; i++)
-							p[i] = Arr[i];
+                int flag_new_word = 0;//0- новое слово 1- найдено в словаре
+                if(stricmp(buff, Arr[0].word) == 0){//проверка на совпадение с первым словом из словаря
+                    Arr[0].n++;
+                    flag_new_word = 0;
+                }
+                else{
+                    if(stricmp(buff, Arr[N-1].word) == 0){//проверка на совпадение с последним словом из словаря
+                        Arr[N-1].n++;
+                        flag_new_word = 0;
+                    }
+                    else{
 
-						free(Arr);
-						Arr = p;
-					}
-				}
+                        if(stricmp(buff, Arr[0].word) < 0){//проверка - расположение слова перед словарем
+                            for(int i = N; i > 0; i--){//сдвиг элементов словаря вправо на 1
+                                Arr[i] = Arr[i-1];
+                            }
+                            Arr[0].word = (char*)malloc(j1+1);//на месте первого создание нового слова
+                            for(int i = 0; i <= j1; i++){
+                                Arr[0].word[i] = buff[i];
+                            }
+                            Arr[0].n = 1;
+                            flag_new_word = 1;
+                        }
+                        else{
+                            if(stricmp(buff, Arr[N-1].word) > 0){//проверка - расположение слова после словаря
+                                Arr[i1].word = (char*)malloc(j1+1);//в конце добавляем новое слово
+                                for(int i = 0; i <= j1; i++){
+                                    Arr[i1].word[i] = buff[i];
+                                }
+                                Arr[i1].n = 1;
+                                flag_new_word = 1;
+                            }
+                            else{//если все условия неверны, то ищем внутри словаря (принцип деления на 2)
+                                flag_new_word = insert(Arr, N, buff, j1+1, 0, N-1);
+                            }
+                        }
+                    }
+                }
+
+
+                j1 = 0;
+                start = 0;
+                if(flag_new_word){//если слова повторное, то наращивать размеры не нужно
+                    i1++;
+                    N++;
+                }
+
+                if (N == max) {//если память полностью заполнена - выделяем в 2 раза больше
+                    max = 2 * max;
+                    Arr = (struct word_keeper*)realloc(Arr, max * sizeof(struct word_keeper));
+						//printf("Memory++\n");
+                }
+
 
 			}
 
 		}
+
+		if (text == EOF) break;//проверка на конец файла должна быть в конце цикла, иначе можно пропустить последнее слово в тексте
 	}
-	quickSort(Arr, 0, N - 1);
+
+	qsort(Arr, N, sizeof(struct word_keeper), compare);//сортировка из библиотеки
 
 	fprintf(outputFile, "Total sentences: %d\n", countSentences);
 
